1e8953a013e8dc96135e4e9add6e30093265000d
MichalBe996 <83282995+MichalBe996@users.noreply.github.com>
2022-01-13 18:06:34 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+
+def isPalindrome(word):
+   reversed_string = "".join(reversed(word))
+   if reversed_string == word:
+       print("This is palindrome.")
+
+   else:
+       print("This is not palindrome.")
+
+
+
